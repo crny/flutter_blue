c53bc242500c536fa5d7c6a792d3977e22a11d1e
crny <crny@qq.com>
2019-11-04 16:55:15 +0800

    old blue api

--- android/src/main/java/com/pauldemarco/flutterblue/FlutterBluePlugin.java
+++ android/src/main/java/com/pauldemarco/flutterblue/FlutterBluePlugin.java
@@ -218,11 +218,11 @@ public class FlutterBluePlugin implements MethodCallHandler, RequestPermissionsR
 
                 // New request, connect and add gattServer to Map
                 BluetoothGatt gattServer;
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    gattServer = device.connectGatt(activity, options.getAndroidAutoConnect(), mGattCallback, BluetoothDevice.TRANSPORT_LE);
-                } else {
-                    gattServer = device.connectGatt(activity, options.getAndroidAutoConnect(), mGattCallback);
-                }
+                // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                //     gattServer = device.connectGatt(activity, options.getAndroidAutoConnect(), mGattCallback, BluetoothDevice.TRANSPORT_LE);
+                // } else {
+                    gattServer = device.connectGatt(activity, false, mGattCallback);
+                // }
                 mDevices.put(deviceId, new BluetoothDeviceCache(gattServer));
                 result.success(null);
                 break;
@@ -654,11 +654,11 @@ public class FlutterBluePlugin implements MethodCallHandler, RequestPermissionsR
         Protos.ScanSettings settings;
         try {
             settings = Protos.ScanSettings.newBuilder().mergeFrom(data).build();
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                startScan21(settings);
-            } else {
+            // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //     startScan21(settings);
+            // } else {
                 startScan18(settings);
-            }
+            // }
             result.success(null);
         } catch (Exception e) {
             result.error("startScan", e.getMessage(), e);
@@ -666,11 +666,11 @@ public class FlutterBluePlugin implements MethodCallHandler, RequestPermissionsR
     }
 
     private void stopScan() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            stopScan21();
-        } else {
+        // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        //     stopScan21();
+        // } else {
             stopScan18();
-        }
+        // }
     }
 
     private ScanCallback scanCallback21;
@@ -746,7 +746,8 @@ public class FlutterBluePlugin implements MethodCallHandler, RequestPermissionsR
         for(int i = 0; i < serviceUuids.size(); i++) {
             uuids[i] = UUID.fromString(serviceUuids.get(i));
         }
-        boolean success = mBluetoothAdapter.startLeScan(uuids, getScanCallback18());
+        // boolean success = mBluetoothAdapter.startLeScan(uuids, getScanCallback18());
+        boolean success = mBluetoothAdapter.startLeScan(getScanCallback18());
         if(!success) throw new IllegalStateException("getBluetoothLeScanner() is null. Is the Adapter on?");
     }
 
